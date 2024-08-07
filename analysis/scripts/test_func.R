b702dfe1ce7b4540b7907fc43697109b6726cd7d
audachang <audachang@gmail.com>
2024-08-07 23:39:59 +0800

    update analysis results and add a test script to bind two data files with partially overlapping columns

--- analysis/scripts/test_func.R
+++ analysis/scripts/test_func.R
@@ -3,13 +3,21 @@ source('asrt_ana_functions.R')
 
 
 sid <- 1
-tasktype <- 'motor'
+#tasktype <- 'motor'
+tasktype <- 'percept'
+
 #seqtype <- 'epoch'
-seqtype <- 'block'
+unitx <- 'block'
 
 finfo <- list_file(tasktype)
-fpth <- select_file(sid, finfo)
-d_add_cond <- add_condition_cols(fpth, tasktype)
-d_add_freq <- assign_freq(d_add_cond)
-d_summary <- create_summ4plot(d_add_freq, seqtype)
-fig <- asrt_plot2(seqtype, tasktype, d_summary, finfo, sid)
+fpth1 <- select_file('sub103', finfo)
+fpth2 <- select_file('sub0103', finfo)
+
+d1 <- import(fpth1)
+d2 <- import(fpth2)
+dbind <- bind_rows(d1, d2)
+
+#d_add_cond <- add_condition_cols(fpth, tasktype)
+#d_add_freq <- assign_freq(d_add_cond)
+#d_summary <- create_summ4plot(d_add_freq, unitx)
+#fig <- asrt_plot2(seqtype, tasktype, d_summary, finfo, sid)
